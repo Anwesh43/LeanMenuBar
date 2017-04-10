eced540a0695da12c9c3b7bc345092f8cab7c6bc
Anwesh43 <anweshthecool0@gmail.com>
2017-04-10 11:14:31 +0530

    Adding MenuBarController

---
+++ leanmenubar/src/main/java/com/anwesome/ui/leanmenubar/MenuBarController.java
@@ -0,0 +1,16 @@
+package com.anwesome.ui.leanmenubar;
+
+import android.app.Activity;
+
+/**
+ * Created by anweshmishra on 10/04/17.
+ */
+public class MenuBarController {
+    private Activity activity;
+    private MenuBarController(Activity activity) {
+        this.activity = activity;
+    }
+    public void show() {
+        
+    }
+}
