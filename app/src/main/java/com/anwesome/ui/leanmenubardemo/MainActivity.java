d4b057f17738b334570b46998fdfa657475ec729
Anwesh43 <anweshthecool0@gmail.com>
2017-04-10 14:43:38 +0530

    using leanmeanubar in app

--- app/src/main/java/com/anwesome/ui/leanmenubardemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/leanmenubardemo/MainActivity.java
@@ -2,6 +2,10 @@ package com.anwesome.ui.leanmenubardemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.anwesome.ui.leanmenubar.LeanMenu;
+import com.anwesome.ui.leanmenubar.MenuBarController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LeanMenu leanMenu1 = new LeanMenu("Hello", new LeanMenu.MenuClickListener() {
+            @Override
+            public void onMenuClick() {
+                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+        LeanMenu leanMenu2 = new LeanMenu("Hi", new LeanMenu.MenuClickListener() {
+            @Override
+            public void onMenuClick() {
+                Toast.makeText(MainActivity.this, "Hi", Toast.LENGTH_SHORT).show();
+            }
+        });
+        LeanMenu leanMenu3 = new LeanMenu("Score", new LeanMenu.MenuClickListener() {
+            @Override
+            public void onMenuClick() {
+                Toast.makeText(MainActivity.this, "Score", Toast.LENGTH_SHORT).show();
+            }
+        });
+        MenuBarController menuBarController = new MenuBarController(this);
+        menuBarController.addLeanMenu(leanMenu1);
+        menuBarController.addLeanMenu(leanMenu2);
+        menuBarController.addLeanMenu(leanMenu3);
+        menuBarController.show();
     }
 }
